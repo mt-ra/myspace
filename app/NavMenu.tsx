0a15fd8261e07d716864f92ff40a1b605eb10e7b
mtra <jeff.j.lu2@gmail.com>
2023-09-05 21:42:43 +1000

    setup

---
+++ app/NavMenu.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./NavMenu.module.css";
+
+function NavMenu() {
+  return <div>Hi</div>;
+}
+
+export default NavMenu;
